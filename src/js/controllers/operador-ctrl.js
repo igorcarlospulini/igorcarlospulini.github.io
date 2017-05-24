21c7f70671aa3531ab279eb927a52291b8968b48
Maykel Rodrigues <maykelrodrigues@live.com>
2017-05-24 18:23:54 -0300

    Inserir o gráfico de timeline por operador

--- src/js/controllers/operador-ctrl.js
+++ src/js/controllers/operador-ctrl.js
@@ -11,25 +11,76 @@ function OperadorCtrl($scope, DataService) {
       $scope.dados = DataService.getItem();
 
       $scope.changedValue = function (id){
-          //var maquinas = useMaquina(id, $scope.dados);
-          desenharTimelineOperador(id, $scope.dados);
-          //desenharPieMaquina(maquinas);
-          //desenharColumnMaquina(maquinas);
+          desenharTimelineLinha(id, $scope.dados);
+
+          var myEl = angular.element( document.querySelector( '#timeline-chart-operador' ) );
+          myEl.empty();
+          $scope.selectOperador = 0;
+
+      }
+
+      $scope.changedValueOperador = function (operador, linha) {
+          desenharTimelineOperador(linha, operador, $scope.dados);
       }
   });
 
 }
 
+function desenharTimelineOperador(idLinha, idOperador, dados) {
+
+  google.charts.load("current", {packages:["timeline"], 'callback': drawTimeline});
+  google.charts.setOnLoadCallback(drawTimeline);
+
+  function drawTimeline() {
+
+      var container = document.getElementById('timeline-chart-operador');
+      var material = new google.visualization.Timeline(container);
+      var dataTable = new google.visualization.DataTable();
+
+      dataTable.addColumn({ type: 'string', id: 'Máquina' });
+      dataTable.addColumn({ type: 'string', id: 'Job' });
+      dataTable.addColumn({ type: 'string', role: 'Tarefa' });
+      dataTable.addColumn({ type: 'date', id: 'Início' });
+      dataTable.addColumn({ type: 'date', id: 'Fim' });
+
+      angular.forEach(dados.distribuicao, function(value, key) {
+          if (value.Linha == idLinha && value.Operador == idOperador)
+            dataTable.addRow([
+                value.Operador ,
+                'Job ' + value.Job,
+                'Tarefa ' + value.Tarefa,
+                new Date(0,0,0,0,value.Inicio, 0),
+                new Date(0,0,0,0,value.Fim, 0)]);
+      });
+
+      var qtd = uniqueArrayValue(dados.distribuicao, 'Linha', idLinha, 'Recurso');
+
+      var options = {
+          height: (qtd.length - 1) * 53.5,
+          timeline: { showBarLabels: false,
+              rowLabelStyle: {fontName: 'Lato', fontSize: 12, color: 'black' }
+          },
+          hAxis: {
+              format: 'H:mm',
+              color: '#FFFFFF'
+          }
+      };
+
+      material.draw(dataTable, options);
+  }
+
+}
+
 //------------------------------------------------------------------------------
 
-function desenharTimelineOperador(idLinha, dados) {
+function desenharTimelineLinha(idLinha, dados) {
 
     google.charts.load("current", {packages:["timeline"], 'callback': drawTimeline});
     google.charts.setOnLoadCallback(drawTimeline);
 
     function drawTimeline() {
 
-        var container = document.getElementById('timeline-chart-operador');
+        var container = document.getElementById('timeline-chart-linha');
         var material = new google.visualization.Timeline(container);
         var dataTable = new google.visualization.DataTable();
 
